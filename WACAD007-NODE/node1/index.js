663f3630621c8d1fab87de957e889aa5c289f9f5
Felipe Barbosa <felipefebl@gmail.com>
2025-08-11 12:41:35 -0400

    parte 2

--- WACAD007-NODE/node1/index.js
+++ WACAD007-NODE/node1/index.js
@@ -1,18 +1,21 @@
-const http = require('http')
-const fs = require('fs')
+const http = require("http")
+const fs = require("fs")
+const dotenv = require("dotenv")
 
-const targetDir = process.argv[2] 
+dotenv.config()
+
+const targetDir = process.argv[2]
+const PORT = process.env.PORT
+console.log(PORT)
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-    fs.readdir(targetDir,(err, files) => {
-        files.forEach( f => res.write(`${f}<br>`))
-        res.end()
-    })
-    
+  res.writeHead(200, { "content-type": "text/html; charset=utf-8" })
+  fs.readdir(targetDir, (err, files) => {
+    files.forEach(f => res.write(`${f}<br>`))
+    res.end()
+  })
 })
 
-server.listen(3000, () => {
-    console.log("servidor rodando")
-  
+server.listen(PORT, () => {
+  console.log("servidor rondando", PORT);
 })
