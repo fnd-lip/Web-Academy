0cc6242ba96ec50790acc5ce6bea47be488d8d8b
Felipe Barbosa <felipefebl@gmail.com>
2025-08-13 16:45:44 -0400

    parte 6

--- WACAD007-NODE/node1/index.js
+++ WACAD007-NODE/node1/index.js
@@ -2,20 +2,38 @@ const http = require("http")
 const fs = require("fs")
 const dotenv = require("dotenv")
 
-dotenv.config({path: `.env.${process.env.NODE_ENV}`, quiet: true})
+const html = require("./src/lib/util.js")
 
-const targetDir = process.argv[2]
-const PORT = process.env.PORT
+dotenv.config({path: `.env.${process.env.NODE_ENV ?? "development"}`, quiet: true})
+
+const PORT = process.env.PORT ?? 4455
+const FOLDER = process.env.FOLDER ?? "public"
 console.log(PORT)
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html; charset=utf-8" })
-  fs.readdir(targetDir, (err, files) => {
-    files.forEach(f => res.write(`${f}<br>`))
-    res.end()
-  })
+
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8"})
+
+  if (req.url === "/"){
+    console.log(`${process.cwd()}/${FOLDER}`);
+    fs.readdir(`${process.cwd()}/${FOLDER}`, (err, files) => {
+     if(err) console.log(err)
+      else files.forEach(file => res.write(html.createLink(file)))
+      res.end()
+    })
+  } else if (req.url !== "/favicon.ico"){
+    const path = `${process.cwd()}/${FOLDER}${req.url}`
+    fs.readFile(path, (err, content) => {
+      if (err) console.log(err, "erro ao ler o arquivo")
+      else {
+        res.write(html.createBackLink())
+        res.write(content.toString()) 
+      }
+      res.end()
+    })
+  }
 })
 
 server.listen(PORT, () => {
-  console.log("servidor rondando", PORT);
+  console.log("servidor rondando", PORT)
 })
