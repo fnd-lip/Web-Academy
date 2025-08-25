8039a148b4351afd1b1fbcefa648d6e4f3bd6771
Felipe Barbosa <felipefebl@gmail.com>
2025-08-25 14:32:52 -0400

    exercício 1

---
+++ WACAD009-Express/Express/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT ?? 6677
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
