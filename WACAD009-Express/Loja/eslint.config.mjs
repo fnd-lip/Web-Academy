7a81b60a74286cdbc0b40a7dd97cb401eb736f5a
Felipe Barbosa <felipefebl@gmail.com>
2025-09-01 22:28:45 -0400

    Exercício 3

---
+++ WACAD009-Express/Loja/eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
+      'no-unused-vars': 'error',
+      eqeqeq: 'error',
+      'no-console': 'off',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-multiple-empty-lines': ['error', { max: 4, maxEOF: 1 }],
+      semi: 'off',
+    },
+  },
+])
